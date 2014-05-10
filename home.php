f857a3a3b692cdcc2ecc86a4e68b3d011789700d
Muhammad Ahsanul Mukmin <ahsanmukmin@gmail.com>
2014-05-10 16:43:53 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php include "cek_session.php"; ?>
+<div class="post">
+	<h2 class="title"><a href="#">Selamat Datang </a></h2>
+	
+	<div class="entry">
+		<p>Selamat datang di sistem input absensi :)</p>
+	</div>
+</div>
+
