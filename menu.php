35645c9f91228f4e56256ecd402aa50573c3ab15
YAFIE WIRA MAHADIKA <yafiiewiira@gmail.com>
2014-05-10 16:38:50 +0700

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,19 @@
+<?php include "cek_session.php"; ?>
+<ul>
+	<li>
+	<div id="kotak">
+		<h2 color="white">MENU UTAMA</h2>
+		
+		<ul>
+			<li><a href="?page=home">Home</a></li>
+			</ul>
+			<ul>
+			<li><a href="?page=kelas">Kelas</a></li>
+			</ul>
+			<ul><li><a href="?page=mahasiswa">Mahasiswa</a></li>
+			</ul><ul><li><a href="?page=absensi">Absensi</a></li>
+			</ul><ul><li><a href="?page=rekap_absensi">Rekap</a></li>
+		</ul>
+	</div>
+	</li>
+</ul>
