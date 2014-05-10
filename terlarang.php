8ef18689bb7204c65b0681abf27c7b96906b2faf
ARIFIN FAJAR <neveenftreveen@gmail.com>
2014-05-10 16:31:10 +0700

    Create terlarang.php

---
+++ terlarang.php
@@ -0,0 +1,15 @@
+<?php include "cek_session.php"; ?>
+<?php
+
+echo "<div class=post>
+	<h2 class=title><a href=#>Peringatan ! </a></h2>
+	
+	<div class=entry>
+		<p>Anda tidak memiliki ha akses untuk menu ini !</p>
+	</div>"; 
+
+?>
+
+</div>
+
+
