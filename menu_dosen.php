f58a11536af9f69cf40596dc47d6641c7ab41f1f
YAFIE WIRA MAHADIKA <yafiiewiira@gmail.com>
2014-05-10 16:37:18 +0700

    Create menu_dosen.php

---
+++ menu_dosen.php
@@ -0,0 +1,19 @@
+<?php include "cek_session.php"; ?>
+<ul>
+	<li>
+	<div id="kotak">
+		<h2 color="white">MENU UTAMA</h2>
+		
+		<ul>
+			<li><a href="?page=home">Home</a></li>
+			</ul>
+			<ul>
+			<li><a href="?page=terlarang">Kelas</a></li>
+			</ul>
+			<ul><li><a href="?page=terlarang">Mahasiswa</a></li>
+			</ul><ul><li><a href="?page=absensi">Absensi</a></li>
+			</ul><ul><li><a href="?page=rekap_absensi">Rekap</a></li>
+		</ul>
+	</div>
+	</li>
+</ul>
