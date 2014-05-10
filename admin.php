e6431485901591127e91f9503149ad39022332e6
Muhammad Ahsanul Mukmin <ahsanmukmin@gmail.com>
2014-05-10 16:38:29 +0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,37 @@
+<?php include "cek_session.php"; ?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" /></head>
+<body >
+<div id="bodybg">
+	<div id="logo" >
+		<h1><a href="#">Tubes Pratikum RPL</a></h1>
+	</div>
+	<div id="header">
+		<div id="menu">
+			<ul>
+				<li><a href="logout.php" onclick="return confirm('Apakah Anda yakin?')">Logout</a></li>
+			</ul>
+		</div>
+		
+	</div>
+	<div id="page" >
+		
+		<div id="content" >
+		<?php include "isi.php";?>
+		</div>
+		
+		<div id="sidebar">
+		<?php include "menu.php";?>
+		</div>
+		<div style="clear: both;">&nbsp;</div>
+	</div>
+
+</div>
+</body>
+</html>
